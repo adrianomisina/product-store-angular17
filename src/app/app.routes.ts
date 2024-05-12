0634efe51ee9595b2fc9273767b6cefd24a6f075
adrianomisina <adriano.misina@gmail.com>
2024-05-12 19:37:13 -0300

    feat: criação de roteamento create-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateProductComponent } from './features/create-product/create-product.component';
 
-export const routes: Routes = [{
-  path:'',
-  component: ListComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+
+  {
+    path: 'create-product',
+    component: CreateProductComponent,
+  },
+];
