7db2b349709b6645fc98590c9f62be170691b8ed
adrianomisina <adriano.misina@gmail.com>
2024-05-29 22:21:14 -0300

    fix: correção do thema na aplicação

--- src/app/features/create-product/create-product.component.ts
+++ src/app/features/create-product/create-product.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule,],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
-export class CreateProductComponent {
 
+export class CreateProductComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required
+    })
+  })
+
+  onSubmit() {
+    this.form.controls.title.value ;
+    this.form.reset()
+  }
 }
